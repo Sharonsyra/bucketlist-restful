9f6e4d8ba286d58c0ef025441059db6c60aabe5a
Sharonsyra <sharonwaithii@gmail.com>
2017-06-05 17:38:36 +0300

    manager commands for createdb and dropdb

--- manage.py
+++ manage.py
@@ -1,14 +1,36 @@
 import os
-from flask_script import Manager # class for handling a set of commands
+import unittest
+
+from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from app import db, create_app
-from app import models
 
 app = create_app(config_name=os.getenv('APP_SETTINGS'))
 migrate = Migrate(app, db)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
+
+@manager.command
+def test():
+    """manager command to run unittests."""
+    tests = unittest.TestLoader().discover('./tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
+
+@manager.command
+def createdb():
+    """ Creates database and its tables """
+    db.create_all()
+    db.session.commit()
+
+@manager.command
+def dropdb():
+    """ Deletes database """
+    if prompt_bool("Are you sure you want to delete?"):
+        db.drop_all()
 
 if __name__ == '__main__':
     manager.run()
