93f6134c8ae8d938f61200ef5c2302d6a336985e
Sharonsyra <sharonwaithii@gmail.com>
2017-06-05 17:40:54 +0300

    folder structure modification

---
+++ config.py
@@ -0,0 +1,40 @@
+import os
+
+
+class Config(object):
+    """Main Config class"""
+    DEBUG = False
+    CSRF_ENABLED = True
+    SECRET = os.getenv('SECRET')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+
+
+class DevelopmentConfig(Config):
+    """Development Configurations inherited from Config."""
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    """Testing Configurations inherited from Config"""
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/test_db'
+    DEBUG = True
+
+
+class StagingConfig(Config):
+    """Staging Configurations inherited from Config."""
+    DEBUG = True
+
+
+class ProductionConfig(Config):
+    """Production Configurations inherited from Config."""
+    DEBUG = False
+    TESTING = False
+
+
+app_config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'staging': StagingConfig,
+    'production': ProductionConfig,
+}
